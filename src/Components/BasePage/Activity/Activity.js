534d12ae2e3ae32293fc6898ca3d537af50ed9fd
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-09-29 02:18:39 +0800

    data loaded to page

--- src/Components/BasePage/Activity/Activity.js
+++ src/Components/BasePage/Activity/Activity.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Activity.css';
 
 const Activity = (props) => {
-    console.log(props.activity);
+    // console.log(props.activity);
+    const { id, picture, duration, detail, name } = props.activity;
+    // console.log(id, duration);
     return (
         <div>
+            <div className="card">
+                <img src={picture} alt="" />
+                <h3>Name: {name}</h3>
+                <h4>Details: {detail}</h4>
+                <p>Skill: <small>Intermediate</small></p>
+                <p>Duration: {duration} min</p>
 
+            </div>
         </div>
     );
 };
